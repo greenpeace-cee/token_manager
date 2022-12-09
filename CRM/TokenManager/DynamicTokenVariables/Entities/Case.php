96fe7fe30b47d5a272209555a965156960d5f2a7
Agiliway Group, Inc <github@agiliway.com>
2022-12-09 14:30:17 +0200

    GP-35450: Update 'getCaseRelationContacts' method.

--- CRM/TokenManager/DynamicTokenVariables/Entities/Case.php
+++ CRM/TokenManager/DynamicTokenVariables/Entities/Case.php
@@ -12,45 +12,27 @@ class CRM_TokenManager_DynamicTokenVariables_Entities_Case extends CRM_TokenMana
   }
 
   public function getCaseRelationContacts($caseId): array {
+    $relatedContactsData = [];
+
     $relationships = \Civi\Api4\Relationship::get(FALSE)
+      ->addSelect('*', 'custom.*', 'contact.*', 'email.*')
+      ->addJoin('Contact AS contact', 'INNER', ['contact_id_a', '=', 'contact.id'])
+      ->addJoin('Email AS email', 'LEFT', ['contact_id_a', '=', 'email.contact_id'], ['email.is_primary', '=', 1])
       ->addWhere('case_id', '=', $caseId)
+      ->setLimit(0)
       ->execute();
 
-    $contactIds = [];
+    foreach ($relationships as $item) {
+      $preparedItem = [];
+      foreach ($item as $fieldName => $value) {
+        $newFieldName = str_replace('.', '_', $fieldName);
+        $preparedItem[$newFieldName] = $value;
+      }
 
-    foreach ($relationships as $relationship) {
-      $contactIds[] = (int) $relationship['contact_id_b'];
-      $contactIds[] = (int) $relationship['contact_id_a'];
+      $relatedContactsData[] = $preparedItem;
     }
 
-    $contactIds = array_unique($contactIds);
-
-    try {
-      $contacts = civicrm_api3('Contact', 'get', [
-        'sequential' => 1,
-        'return' => ["id", "display_name", "addressee_display", "email", "phone", "last_name", "first_name"],
-        'id' => ['IN' => $contactIds],
-        'options' => ['limit' => 0],
-      ]);
-    } catch (CiviCRM_API3_Exception $e) {
-      return [];
-    }
-
-    $relatedContacts = [];
-
-    foreach ($contacts['values'] as $contact) {
-      $relatedContacts[] = [
-        'contact_id' => $contact['contact_id'],
-        'display_name' => $contact['display_name'],
-        'first_name' => !empty($contact['first_name']) ? $contact['first_name'] : '',
-        'last_name' => !empty($contact['last_name']) ? $contact['last_name'] : '',
-        'addressee_display' => !empty($contact['addressee_display']) ? $contact['addressee_display'] : '',
-        'email' => !empty($contact['email']) ? $contact['email'] : '',
-        'phone' => !empty($contact['phone']) ? $contact['phone'] : '',
-      ];
-    }
-
-    return $relatedContacts;
+    return $relatedContactsData;
   }
 
 
