9a6225544270a3d1b55698378a5b5b77badca5fb
Patrick Figel <pfigel@greenpeace.org>
2023-02-16 10:24:31 +0100

    GP-35450 Add phone to relationship token

--- CRM/TokenManager/DynamicTokenVariables/Entities/Case.php
+++ CRM/TokenManager/DynamicTokenVariables/Entities/Case.php
@@ -15,17 +15,18 @@ class CRM_TokenManager_DynamicTokenVariables_Entities_Case extends CRM_TokenMana
     $relatedContactsData = [];
 
     $relationships = \Civi\Api4\Relationship::get(FALSE)
-      ->addSelect('*', 'custom.*', 'contact.*', 'email.*')
+      ->addSelect('*', 'custom.*', 'contact.*', 'email.*', 'phone.*', 'relationship_type_id:name')
       ->addJoin('Contact AS contact', 'INNER', ['contact_id_a', '=', 'contact.id'])
       ->addJoin('Email AS email', 'LEFT', ['contact_id_a', '=', 'email.contact_id'], ['email.is_primary', '=', 1])
+      ->addJoin('Phone AS phone', 'LEFT', ['contact_id_a', '=', 'phone.contact_id'], ['phone.is_primary', '=', 1])
       ->addWhere('case_id', '=', $caseId)
-      ->setLimit(0)
+      ->addWhere('is_active', '=', TRUE)
       ->execute();
 
     foreach ($relationships as $item) {
       $preparedItem = [];
       foreach ($item as $fieldName => $value) {
-        $newFieldName = str_replace('.', '_', $fieldName);
+        $newFieldName = str_replace(['.', ':'], '_', $fieldName);
         $preparedItem[$newFieldName] = $value;
       }
 
